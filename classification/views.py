fbbbcf88553ba0764e07d7ac3752c3c8fb656142
Bryann Torres <arleyro91@gmail.com>
2023-04-21 22:55:47 -0500

    Created transformers interface

--- classification/views.py
+++ classification/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from classification.utils import classify_tweets_with_twitter_api
+
 
 def index(request):
     return render(request, 'index.html')
@@ -11,4 +13,9 @@ def classify(request):
     if not api_key or not api_secret:
         return render(request, 'index.html', {'error': 'Please provide both API key and secret.'})
     print(api_key, api_secret)
+    try:
+        classify_tweets_with_twitter_api(api_key, api_secret)
+    except Exception as e:
+        print(e)
+        return render(request, 'index.html', {'error': 'Something went wrong. Please try again.'})
     return render(request, 'index.html', {'api_key': api_key, 'api_secret': api_secret})
